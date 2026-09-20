f5bb6eff5c442697c198296e12382f8d1a87a554
nikkilea324-png <nikkilea324@gmail.com>
2026-09-19 20:37:48 -0400

    Add GdUnit4 gameplay and Android touch tests

---
+++ tests/main_test.gd
@@ -0,0 +1,32 @@
+extends GdUnitTestSuite
+
+var game: Node2D
+
+func before_test() -> void:
+	game = load("res://scenes/Main.tscn").instantiate()
+	add_child(game)
+
+func after_test() -> void:
+	if is_instance_valid(game):
+		game.queue_free()
+
+func test_world_map_selects_territory() -> void:
+	assert_that(game.mode).is_equal("world")
+	game._handle_click(Vector2(100, 200))
+	assert_that(game.mode).is_equal("territory")
+	assert_that(game.selected_territory).is_equal("Crownspine")
+
+func test_android_touch_event_selects_territory() -> void:
+	var touch := InputEventScreenTouch.new()
+	touch.position = Vector2(400, 400)
+	touch.pressed = true
+	game._input(touch)
+	assert_that(game.mode).is_equal("territory")
+	assert_that(game.selected_territory).is_equal("Greenvale")
+
+func test_territory_can_reach_cave_and_enter_dungeon() -> void:
+	game._handle_click(Vector2(100, 200))
+	assert_that(game.mode).is_equal("territory")
+	game.hero_cell = game.cave_cell
+	game._handle_click(Vector2(1100, 650))
+	assert_that(game.mode).is_equal("dungeon")
