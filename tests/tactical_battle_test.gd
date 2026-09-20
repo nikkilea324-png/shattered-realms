bf8b1900671c3540942b9ae3a01025edb2f42ac5
nikkilea324-png <nikkilea324@gmail.com>
2026-09-19 23:00:02 -0400

    test: use supported GdUnit comparison assertions

--- tests/tactical_battle_test.gd
+++ tests/tactical_battle_test.gd
@@ -5,14 +5,14 @@ func test_tactical_battle_builds_from_core_unit_data() -> void:
 	battle.setup({"spearmen":2,"archers":2,"knights":1},{"riders":2,"shieldguard":1},"forest")
 	assert_that(battle.player_units.has("spearmen")).is_true()
 	assert_that(battle.enemy_units.has("riders")).is_true()
-	assert_that(battle.get_force_strength("player")).is_greater_than(0)
+	assert_that(battle.get_force_strength("player") > 0).is_true()
 
 func test_spearmen_counter_cavalry() -> void:
 	var battle := TacticalBattleState.new()
 	battle.setup({"spearmen":2},{"riders":2},"open")
 	var result := battle.attack("player","spearmen","riders")
 	assert_that(result.ok).is_true()
-	assert_that(result.damage).is_greater_than(1)
+	assert_that(result.damage > 1).is_true()
 
 func test_forest_improves_rangers() -> void:
 	var forest := TacticalBattleState.new()
@@ -21,7 +21,7 @@ func test_forest_improves_rangers() -> void:
 	open.setup({"rangers":1},{"militia":2},"open")
 	var forest_hit := forest.attack("player","rangers","militia")
 	var open_hit := open.attack("player","rangers","militia")
-	assert_that(forest_hit.damage).is_greater_than(open_hit.damage)
+	assert_that(forest_hit.damage > open_hit.damage).is_true()
 
 func test_battle_detects_victory() -> void:
 	var battle := TacticalBattleState.new()
